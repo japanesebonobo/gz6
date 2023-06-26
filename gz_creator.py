d37d0ad5a41d1a2ca6c04d30891f0607cf3c4793
yutayosh <yutayosh@yahoo-corp.jp>
2023-06-26 13:53:30 +0900

    議事録を生成するpyファイル作成

--- gz_creator.py
+++ gz_creator.py
@@ -1,5 +1,13 @@
 import openai
 
+system_template = """会議の書き起こしが渡されます。
+
+この会議のサマリーをMarkdown形式で作成してください。サマリーは、以下のような形式で書いてください。
+
+- 会議の目的
+- 会議の内容
+- 会議の結果"""
+
 # APIキーの入力を受け取る
 api_key = input("APIキーを入力してください: ")
 openai.api_key = api_key
@@ -10,7 +18,10 @@ user_input = input("メッセージを入力してください: ")
 # ChatCompletionのリクエストにユーザーメッセージを追加
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo", 
-    messages=[{"role": "user", "content": user_input}]
+    messages=[
+        {"role": "system", "content": system_template},
+        {"role": "user", "content": user_input}
+    ]
 )
 
 # 応答の出力
